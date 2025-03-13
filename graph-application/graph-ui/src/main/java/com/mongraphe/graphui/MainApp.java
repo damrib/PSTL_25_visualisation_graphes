93dac499c77255c90387dd578c5f25547fe5eab4
fa214153 <awwal-ronald-oriade.fagbehouro@etu.univ-lehavre.fr>
2025-03-13 23:56:44 +0100

    intégration du makefile

--- graph-application/graph-ui/src/main/java/com/mongraphe/graphui/MainApp.java
+++ graph-application/graph-ui/src/main/java/com/mongraphe/graphui/MainApp.java
@@ -1,5 +1,8 @@
 package com.mongraphe.graphui;
 
+import java.io.File;
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,6 +10,29 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
+	
+	public static void runMake(String directory) {
+        try {
+            // Initialisation du ProcessBuilder
+            ProcessBuilder builder = new ProcessBuilder("make");
+
+            // Définir le répertoire où exécuter la commande
+            builder.directory(new File(directory));
+
+            // Rediriger les flux de sortie et d'erreur pour voir les logs
+            builder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+            builder.redirectError(ProcessBuilder.Redirect.INHERIT);
+
+            // Lancer le processus
+            Process process = builder.start();
+
+            // Attendre la fin de l'exécution
+            int exitCode = process.waitFor();
+            System.out.println("Make terminé avec le code : " + exitCode);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void start(Stage primaryStage) {
@@ -22,6 +48,9 @@ public class MainApp extends Application {
     }
 
     public static void main(String[] args) {
+    	runMake("./");
+    	String libnative = System.getProperty("user.dir") + "/out/libnative.so";
+        System.load(libnative);
         launch(args);
     }
 }
